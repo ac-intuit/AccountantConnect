7612f6d0170407777a8ed514ef4cea8792997064
karthik <ac-intuit@gmail.com>
2014-02-20 12:07:32 +0530

    java script validation for acmainpage.html

--- WebContent/js/ac-signup.js
+++ WebContent/js/ac-signup.js
@@ -1,7 +1,31 @@
 function validateFieldsOnSignUpForm(){
-   var accountantName = $("#accountant_name").val();
-   if(accountantName == ""){
-	   $("#accountantEmailNoDataErr").show();
+   var accountantEmail = $("#accountant_email").val();
+   
+  
+   
+   var atpos=accountantEmail.indexOf("@");
+   var dotpos=accountantEmail.lastIndexOf(".");
+   
+   var pattern = new RegExp("/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/");
+   if(atpos<1 || dotpos<atpos+2 || dotpos+2>=accountantEmail.length){
+	   $("#infoTextMsg").hide();
+	   $("#accountantEmailInvalidFormat").show();
+	   $("#accountant_email").focus();
+	   return false;
    }
-   return false;
-}
+   
+   else {
+	   $("#accountantEmailInvalidFormat").hide();
+	   return true;
+   }
+  
+  
+   
+    
+	    
+   }
+   
+  
+ 
+	   
+  
